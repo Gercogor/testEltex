4712e49e74da5509f220dafba984849644405d8b
Gercogor <93824893+Gercogor@users.noreply.github.com>
2023-02-28 10:56:06 +0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,36 @@
+      (function rndStr() {
+        const litters = 'qwertyuiopasdfghjklzxcvbnm'
+        const numbers = '0123456789'
+        const symbols = '+-_$~'
+        const blocked = 'cpsx'
+        let maxLength = (numbers+litters+symbols).length
+        let str: any = '';
+        let n: any = prompt('string`s length');
+        while (str.length < n) {
+          let a = (numbers+litters+symbols)[Math.floor(Math.random() * maxLength)]
+          if (!blocked.includes(a))
+            str += a;
+        }
+        alert(str);
+        let symb1 = prompt('Replace litters');
+        let i = 0;
+        str = str.split('');
+        while (i<str.length) {
+          if(litters.includes(str[i])) str[i] = symb1
+          i++
+        }
+        let symb2 = prompt('Replace numbers');
+        i = 0;
+        while (i<str.length) {
+          if(numbers.includes(str[i])) str[i] = symb2
+          i++
+        }
+        str = str.join('')
+        alert(str)
+        alert(
+          `count of symbols 1 = ${str.length - str.split(symb1).join('').length} ;
+           count of symbols 2 = ${str.length - str.split(symb2).join('').length} ;
+           count of unchanged symbols =  ${str.split(symb1).join('').split(symb2).join('').length} ;
+          `
+        )
+      })()
